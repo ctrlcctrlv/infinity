8756bd8544d8a8dda643fa0d90228eb1e11bcbb1
8chan <admin@8chan.co>
2014-04-17 00:41:33 +0000

    Fix stylechooser

--- templates/main.js
+++ templates/main.js
@@ -293,8 +293,6 @@ function ready() {
 
 var post_date = "{{ config.post_date }}";
 
-onready(init);
-
 {% if config.google_analytics %}{% raw %}
 
 var _gaq = _gaq || [];_gaq.push(['_setAccount', '{% endraw %}{{ config.google_analytics }}{% raw %}']);{% endraw %}{% if config.google_analytics_domain %}{% raw %}_gaq.push(['_setDomainName', '{% endraw %}{{ config.google_analytics_domain }}{% raw %}']){% endraw %}{% endif %}{% if not config.google_analytics_domain %}{% raw %}_gaq.push(['_setDomainName', 'none']){% endraw %}{% endif %}{% raw %};_gaq.push(['_trackPageview']);(function() {var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);})();{% endraw %}{% endif %}
