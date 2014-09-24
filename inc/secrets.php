e94ae94f6cbca31030c0df3d20ecdec023468927
8chan <admin@8chan.co>
2014-09-24 20:17:59 +0000

    Add extra non-synced file for 8chan.co secrets (passwords etc)

---
+++ inc/secrets.php
@@ -0,0 +1,4 @@
+<?php
+	$config['db']['password'] = '57x9RaVc8HQW2Ga0i74w';
+	$config['cookies']['salt'] = 'UiFLKolfLXUIZZCDQIFOQT1fjCvdyvWqIx3olOnJq0TJpbCAeC';
+	$config['secure_trip_salt'] = 'sK7A4umU4dU0875MAi012239p4WdE7A699032MJWxspSOqb39X';
