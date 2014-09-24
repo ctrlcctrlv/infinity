c291952477d27f618af03e38a334ef990a9f3dc9
anonfagola <anonfagola@users.noreply.github.com>
2014-09-24 16:41:23 -0700

    Create 404.php

---
+++ 404.php
@@ -0,0 +1,20 @@
+<?php
+
+include "inc/functions.php";
+
+$errorimages = array("http://www.submitawebsite.com/blog/wp-content/uploads/2010/06/404.png",
+					"http://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Georgia_404.svg/750px-Georgia_404.svg.png");
+
+$errorimage = $errorimages[array_rand($errorimages)];
+
+$page = <<<EOT
+	<center>
+		<div class="ban">
+			<h2>404 Not Found</h2>
+			
+			<img src="{$errorimage}" style="width: 700px;">
+		</div>
+	</center>
+EOT;
+
+echo Element("page.html", array("config" => $config, "body" => $page, "title" => ""));
