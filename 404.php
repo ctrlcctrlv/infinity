581fcaa6d7b54c2acc24dd078c004ec2227085d3
anonfagola <anonfagola@users.noreply.github.com>
2014-09-25 15:40:43 -0700

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,11 +1,19 @@
 <?php
 
 include "inc/functions.php";
+include "inc/cache.php";
 
 if(!file_exists("404/"))
 	mkdir("404/");
 
+$cache = new Cache;
+$cache->init();
+
+if($cache->get("404glob") == false){
 $files = glob("404/*.*");
+$cache->set("404glob", $files);
+}else
+$files = $cache->get("404glob");
 
 if(count($files) == 0)
 	$errorimage = "";
