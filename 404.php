3a29672b0305d1e0300de9e9e86f503d410b67f0
anonfagola <anonfagola@users.noreply.github.com>
2014-09-24 17:02:40 -0700

    Update 404.php
    Made it loop through directory

--- 404.php
+++ 404.php
@@ -2,17 +2,24 @@
 
 include "inc/functions.php";
 
-$errorimages = array("http://www.submitawebsite.com/blog/wp-content/uploads/2010/06/404.png",
-					"http://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Georgia_404.svg/750px-Georgia_404.svg.png");
+if(!file_exists("404/"))
+	mkdir("404/");
 
-$errorimage = $errorimages[array_rand($errorimages)];
+$files = glob("404/*.*");
+
+if(count($files) == 0)
+	$errorimage = "";
+else
+	$errorimage = $files[array_rand($files)];
 
 $page = <<<EOT
-	<div class="ban">
-		<h2 style="text-align: center">404 Not Found</h2>
-		
-		<img src="{$errorimage}" style="width: 700px;">
-	</div>
+	<center>
+		<div class="ban">
+			<h2>404 Not Found</h2>
+			
+			<img src="{$errorimage}" style="width: 700px;">
+		</div>
+	</center>
 EOT;
 
 echo Element("page.html", array("config" => $config, "body" => $page, "title" => ""));
