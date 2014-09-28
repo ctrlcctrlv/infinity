1a868a5e9a8e29a56aaefe8cf0ece83ff36c91a2
anonfagola <anonfagola@users.noreply.github.com>
2014-09-27 17:43:15 -0700

    Create user-filter.js

---
+++ js/options/user-filter.js
@@ -0,0 +1,12 @@
+//>onclick
+function updateFilter(){
+	localStorage["filter"] = $("#filter_ta").val();
+	location.reload();
+}
+
+Options.add_tab("filter", "times", "Filter", "<textarea id='filter_ta' style='font-size: 12px; position: absolute; top: 35px; bottom: 35px; width: calc(100% - 12px); margin: 0px; padding: 0px; border: 1px solid black; left: 5px; right: 5px;'>"+localStorage["filter"].replace(/&/g, "&amp;")
+         .replace(/</g, "&lt;")
+         .replace(/>/g, "&gt;")
+         .replace(/"/g, "&quot;")
+         .replace(/'/g, "&#039;")+"</textarea>"
+		 +'<button onclick="updateFilter()" style="position: absolute; height: 25px; bottom: 5px; width: calc(100% - 10px); left: 5px; right: 5px;">Update filters</button>');
