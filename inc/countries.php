ba7cc64bbbf70d569ab4dba0c799233ee3130097
Bui <bui@bui.pm>
2014-09-29 07:30:45 +0900

    fix languages

--- inc/countries.php
+++ inc/countries.php
@@ -3,26 +3,28 @@
 function get_country( $code ){
     $country = '';
     if( $code == 'en' ) $country = 'English';
-    if( $code == 'cs' ) $country = 'Czech';
-    if( $code == 'da' ) $country = 'Danish';
+    if( $code == 'cz' ) $country = 'Czech';
+    if( $code == 'dk' ) $country = 'Danish';
     if( $code == 'de' ) $country = 'German';
+    if( $code == 'au' ) $country = 'Australian';
     if( $code == 'eo' ) $country = 'Esperanto';
     if( $code == 'es' ) $country = 'Spanish';
     if( $code == 'fi' ) $country = 'Finnish';
     if( $code == 'fr' ) $country = 'French';
     if( $code == 'hu' ) $country = 'Hungarian';
     if( $code == 'it' ) $country = 'Italian';
-    if( $code == 'ja' ) $country = 'Japanese';
+    if( $code == 'jp' ) $country = 'Japanese';
     if( $code == 'jbo' ) $country = 'Lojban';
     if( $code == 'lt' ) $country = 'Lithuanian';
     if( $code == 'lv' ) $country = 'Latvian';
-    if( $code == 'nb' ) $country = 'Norwegian';
+    if( $code == 'no' ) $country = 'Norwegian';
     if( $code == 'nl' ) $country = 'Dutch';
     if( $code == 'pl' ) $country = 'Polish';
+    if( $code == 'br' ) $country = 'Brazilian';
     if( $code == 'pt' ) $country = 'Portuguese';
     if( $code == 'ru' ) $country = 'Russian';
     if( $code == 'sk' ) $country = 'Slovak';
-    if( $code == 'zh' ) $country = 'Chinese';
+    if( $code == 'tw' ) $country = 'Taiwanese';
 
     if( $country == '') $country = $code;
     return $country;
