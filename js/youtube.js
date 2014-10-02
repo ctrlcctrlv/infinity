d8614694a48ced8f15c513395bb65ca4e3d656c0
undido <firekid109@hotmail.com>
2014-10-01 22:05:08 -0300

    took variables off global scope, refractored variable names replaced redundant string prototype with function, now uses options api

--- js/youtube.js
+++ js/youtube.js
@@ -30,144 +30,111 @@
 *	};
 *
 */
-$(document)
-    .ready(function () {
-        var settings = new script_settings('youtube_embed'),
-            youtubeExptext = /(?:youtube.com\/watch\?[^>]*v=|youtu.be\/)([\w_-]{11})(?:(?:#|\?|&amp;)a?t=([ms\d]+)|[^"])*.*?/i,
-            PlayerWidth = (typeof settings.get('player_width') == "undefined") ? "420px" : settings.get('player_width'),
-            PlayerHeight = (typeof settings.get('player_height') == "undefined") ? "315px" : settings.get('player_height');
-			autoPlay = (typeof settings.get('autoplay') == "undefined") ? "1" : ((settings.get('autoplay') == true) ? "1":"0" );
+$(document).ready(function () {
+	if (window.Options && window.Options.get_tab("general")){
+        var settings = new script_settings('youtube_embed');
+			//expression for finding a youtube url
+        var youtubeExptext = /(?:youtube.com\/watch\?[^>]*v=|youtu.be\/)([\w_-]{11})(?:(?:#|\?|&amp;)a?t=([ms\d]+)|[^"])*.*?/i;
+        var PlayerWidth = (typeof settings.get('player_width') == "undefined") ? "420px" : settings.get('player_width');
+        var PlayerHeight = (typeof settings.get('player_height') == "undefined") ? "315px" : settings.get('player_height');
+		var autoPlay = (typeof settings.get('autoplay') == "undefined") ? "1" : ((settings.get('autoplay') == true) ? "1":"0" );
 
 
-
-        String.prototype.youTubeparsetext = function () {
-            var match = this.match(youtubeExptext);
+		
+		var findYoutubeLink = function(string){
+			if (typeof string == "undefined")
+				return false;
+		
+            var match = string.match(youtubeExptext);
             if (match && match[1].length == 11) {
                 return match[1];
             } else {
-                return String('false');
+                return false;
             }
-        };
-
+		};
 
         var YouTubeBox = function () {
             //video url
-            var yt_url = this.href;
+            var yt_url = $(this).attr("href");
             //video id
-            yt_id = yt_url.youTubeparsetext();
+            var yt_id = findYoutubeLink(yt_url);
 
-            var $button = $("<div />")
-                .append($($("<a/>", {
+            var $button = $("<a/>", {
                         "rel": "nofollow",
                         "target": "_BLANK",
                         "text": "Embed"
-                    }))
-                    .clone())
-                .html();
+                    });
 
-            $youtubeBox = $('<span/>', {
-                "class": 'embedbutton',
-                "data-opened": "false",
-                "data-youtubeid": yt_id,
-                "css": {
-                    "cursor": "pointer"
-                }
+            var $youtubeEmbButtonContainer = $('<span/>', {
+					"class": 'embedbutton',
+					"data-opened": "false",
+					"data-youtubeid": yt_id,
+					"css": {
+								"cursor": "pointer"
+							}
+				});
+			$youtubeEmbButtonContainer.append(" [");
+            $youtubeEmbButtonContainer.append($button);
+			$youtubeEmbButtonContainer.append("]");
+            $youtubeEmbButtonContainer.click(clickEmbedButton);
 
-
-            });
-
-            $youtubeBox.html(" [" + $button + "]");
-
-            $youtubeBox.click(clickEmbed);
-
-            $(this)
-                .after($youtubeBox);
+            $(this).after($youtubeEmbButtonContainer);
         };
 
+        var clickEmbedButton = function () {
+            var yt_id = $(this).attr("data-youtubeid");
+            var yt_ind = $(this).index();
 
-
-
-        var clickEmbed = function () {
-            yt_id2 = $(this)
-                .attr("data-youtubeid");
-            yt_ind = $(this)
-                .index();
-
-
-            if ($(this)
-                .attr("data-opened") == "false") {
-                $(this)
-                    .after('<span data-ytid="' + yt_id2 + '" data-ytind="' + yt_ind + '" class="youtube-box"></br><iframe style="display:inline-block;width:' + PlayerWidth + ';height:' + PlayerHeight + ';border:none;" class="youtube-frame" src="//www.youtube.com/embed/' + yt_id2 + '?origin=' + document.location.host + '&autoplay="' + autoPlay + '></iframe></span>');
-                $(this)
-                    .attr("data-opened", "true");
-                $(this)
-                    .children('a')
-                    .text("Close");
+            if ($(this).attr("data-opened") == "false") {
+                $(this).after('<span data-ytid="' + yt_id + '" data-ytind="' + yt_ind + '" class="youtube-box"></br><iframe style="display:inline-block;width:' + PlayerWidth + ';height:' + PlayerHeight + ';border:none;" class="youtube-frame" src="//www.youtube.com/embed/' + yt_id + '?origin=' + document.location.host + '&autoplay="' + autoPlay + '></iframe></span>');
+                $(this).attr("data-opened", "true");
+                $(this).children('a').text("Close");
             } else {
 
-                a = $.find("[data-ytid='" + yt_id2 + "']" + "[data-ytind='" + yt_ind + "']");
-                $(a)
-                    .remove();
-                $(this)
-                    .attr("data-opened", "false");
-                $(this)
-                    .children('a')
-                    .text("Embed");
+                var a = $.find("[data-ytid='" + yt_id + "']" + "[data-ytind='" + yt_ind + "']");
+                $(a).remove();
+                $(this).attr("data-opened", "false");
+                $(this).children('a').text("Embed");
             }
 
         };
-
-
 
         var YouTubeInit = function () {
-            var text = $(this)
-                .html();
-            var isYoutubeLink = text.youTubeparsetext();
+            var text = $(this).html();
+            var isYoutubeLink = findYoutubeLink(text);
 
-            if (isYoutubeLink != 'false') {
-                $(this)
-                    .each(YouTubeBox);
-
+            if (isYoutubeLink != false) {
+                $(this).each(YouTubeBox);
             }
         };
-
-
-
-
-        var disableYouTubeBox = function () {
-            $('.embedbutton')
-                .each(function () {
-                    $(this)
-                        .remove();
-                });
-
-            $('.youtube-box')
-                .each(function () {
-                    $(this)
-                        .remove();
-                });
-
-
-
+		
+        var disableYouTubeEmbed = function () {
+            $('.embedbutton').each(function () {
+				$(this).remove();
+			});
+            $('.youtube-box').each(function () {
+				$(this).remove();
+			});
         };
 
-
-
-
-        emb_vid = localStorage['embvid'] ? true : false;
-
+		var emb_vid = localStorage['embvid'] ? true : false;
+		var selector, event;
 		
-		$('.options_tab:first').append('<div><label id="emb-vid"><input type="checkbox" id="emb-toggle" class="menuOption" data-option="embvid"> Embed youtube links</label></div>');
+		
+		
+		Options.extend_tab('general', '<div><label id="toggle-emb-vid"><input type="checkbox"> Embed youtube links</label></div>');
+		
+		selector = "#toggle-emb-vid";
+		event = "click";
+		
+		$('#toggle-emb-vid>input').prop("checked", !emb_vid);
 
-		$('#emb-toggle').prop("checked", emb_vid ? false:true);
 
-
-        $('#emb-toggle')
-            .click(function () {
-			
+        $('#toggle-emb-vid>input').on(event,function () {
+		console.log($(this).prop("checked"));
 				if (!$(this).prop("checked")){
 					emb_vid = true;
-					disableYouTubeBox();
+					disableYouTubeEmbed();
 					localStorage['embvid'] = true;
 				} else {
 					emb_vid = false;
@@ -176,20 +143,12 @@ $(document)
 				}
             });
 			
-	
         if (!emb_vid){
-           $(".body a")
-                .each(YouTubeInit);
+           $(".body a").each(YouTubeInit);
 		}
-
-
-        $(document)
-            .bind('new_post', function (e, post) {
+        $(document).bind('new_post', function (e, post) {
                 if (!emb_vid)
-                    $(post)
-                        .find('.body a')
-                        .each(YouTubeInit);
+                    $(post).find('.body a').each(YouTubeInit);
             });
-
-
-    });
+	}
+});
