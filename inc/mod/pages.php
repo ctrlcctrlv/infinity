47e81b7e9bedeb79d8f95ff2677ec12b543b9297
8chan <admin@8chan.co>
2014-10-06 18:02:50 -0700

    Fix spoiler image not working

--- inc/mod/pages.php
+++ inc/mod/pages.php
@@ -1588,7 +1588,7 @@ function mod_spoiler_image($board, $post, $file) {
 	$size_spoiler_image = @getimagesize($config['spoiler_image']);
 	file_unlink($config['dir']['img_root'] . $board . '/' . $config['dir']['thumb'] . $files[$file]->thumb);
 	$files[$file]->thumb = 'spoiler';
-	$files[$file]->thumbwidth = $size_spoiler[0];
+	$files[$file]->thumbwidth = $size_spoiler_image[0];
 	$files[$file]->thumbheight = $size_spoiler_image[1];
 	
 	// Make thumbnail spoiler
