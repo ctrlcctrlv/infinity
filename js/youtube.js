668b374e4386c66a56f41add7247e15fcce5e25b
undido <firekid109@hotmail.com>
2014-10-08 21:29:00 -0300

    License change

--- js/youtube.js
+++ js/youtube.js
@@ -1,24 +1,10 @@
 /*
 * youtube.js
-
-    Copyright (C) 2014  undido
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-	
-	PURE JAVASCRIPT YOUTUBE EMBBEDING
-	
-	
+* https://github.com/ctrlcctrlv/8chan/blob/master/js/youtube.js
+*
+* Released under the MIT license
+* Copyright (c) 2014 Undido
+* 
 *
 * Optional: 
 *	If you use settings.js(https://github.com/savetheinternet/Tinyboard/blob/master/js/settings.js)
