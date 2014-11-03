1a259a3cd411ec993c77458fc87d8873ae63b6fa
topkek <topkek@users.noreply.github.com>
2014-11-03 23:40:54 +0000

    Update post-hover.js

--- js/post-hover.js
+++ js/post-hover.js
@@ -16,15 +16,15 @@
 onready(function(){
 	var dont_fetch_again = [];
 	init_hover = function() {
-		var $link = $(this);
+		var link = $(this);
 		
 		var id;
 		var matches;
 
-                if ($link.is('[data-thread]')) {
-                        id = $link.attr('data-thread');
+                if (link.is('[data-thread]')) {
+                        id = link.attr('data-thread');
                 }
-		else if(matches = $link.text().match(/^>>(?:>\/([^\/]+)\/)?(\d+)$/)) {
+		else if(matches = link.text().match(/^>>(?:>\/([^\/]+)\/)?(\d+)$/)) {
 			id = matches[2];
 		}
 		else {
@@ -36,39 +36,33 @@ onready(function(){
 			board = board.parent();
 		}
 		var threadid;
-		if ($link.is('[data-thread]')) threadid = 0;
+		if (link.is('[data-thread]')) threadid = 0;
 		else threadid = board.attr('id').replace("thread_", "");
 
 		board = board.data('board');
 
 		var parentboard = board;
 		
-		if ($link.is('[data-thread]')) parentboard = $('form[name="post"] input[name="board"]').val();
+		if (link.is('[data-thread]')) parentboard = $('form[name="post"] input[name="board"]').val();
 		else if (matches[1] !== undefined) board = matches[1];
 
-		var $post = false;
+		var post = false;
 		var hovering = false;
-		var hovered_at;
-		$link.hover(function(e) {
+		link.hover(function(e) {
 			hovering = true;
-			hovered_at = {'x': e.pageX, 'y': e.pageY};
 			
-			var start_hover = function($link) {
-				if($.contains($post[0], $link[0])) {
-					// link links to itself or to op; ignore
-				}
-				else if($post.is(':visible') &&
-						$post.offset().top >= $(window).scrollTop() &&
-						$post.offset().top + $post.height() <= $(window).scrollTop() + $(window).height()) {
+			var start_hover = function(link) {					
+				if(post.is(':visible') &&
+						post.offset().top >= $(window).scrollTop() &&
+						post.offset().top + post.height() <= $(window).scrollTop() + $(window).height()) {
 					// post is in view
-					$post.addClass('highlighted');
+					post.addClass('highlighted');
 				} else {
-					var $newPost = $post.clone();
-					$newPost.find('>.reply, >br').remove();
-					$newPost.find('span.mentioned').remove();
-					$newPost.find('a.post_anchor').remove();
+					var newPost = post.clone();
+					newPost.find('>.reply, >br').remove();
+					newPost.find('a.post_anchor').remove();
 
-					$newPost
+					newPost
 						.attr('id', 'post-hover-' + id)
 						.attr('data-board', board)
 						.addClass('post-hover')
@@ -78,18 +72,62 @@ onready(function(){
 						.css('position', 'absolute')
 						.css('font-style', 'normal')
 						.css('z-index', '100')
+						.css('left', '0')
 						.addClass('reply').addClass('post')
-						.insertAfter($link.parent())
-
-					$link.trigger('mousemove');
+						.insertAfter(link.parent())
+						
+					// shrink expanded images
+					newPost.find('div.file a[data-expanded="true"]').each(function() {
+						var thumb = $(this).data('src');
+						$(this).find('img.post-image').attr('src', thumb);
+					});
+					
+					var previewWidth = newPost.outerWidth(true);
+					var widthDiff = previewWidth - newPost.width();
+					var linkLeft = link.offset().left;
+					var left, top;
+					
+					if (linkLeft < $(document).width() * 0.7) {
+						left = linkLeft + link.width();
+						if (left + previewWidth > $(window).width()) {
+							newPost.css('width', $(window).width() - left - widthDiff);
+						}
+					} else {
+						if (previewWidth > linkLeft) {
+							newPost.css('width', linkLeft - widthDiff);
+							previewWidth = linkLeft;
+						}
+						left = linkLeft - previewWidth;
+					}
+					newPost.css('left', left);
+					
+					top = link.offset().top - 10;
+					
+					var scrollTop = $(window).scrollTop();
+					if (link.is("[data-thread]")) {
+						scrollTop = 0;
+						top -= $(window).scrollTop();	
+					}
+					
+					if(top < scrollTop + 15) {
+						top = scrollTop;
+					} else if(top > scrollTop + $(window).height() - newPost.height() - 15) {
+						top = scrollTop + $(window).height() - newPost.height() - 15;
+					}
+					
+					if (newPost.height() > $(window).height()) {
+						top = scrollTop;
+					}
+					
+					newPost.css('top', top);
 				}
 			};
 			
-			$post = $('[data-board="' + board + '"] div.post#reply_' + id + ', [data-board="' + board + '"]div#thread_' + id);
-			if($post.length > 0) {
+			post = $('[data-board="' + board + '"] div.post#reply_' + id + ', [data-board="' + board + '"]div#thread_' + id);
+			if(post.length > 0) {
 				start_hover($(this));
 			} else {
-				var url = $link.attr('href').replace(/#.*$/, '');
+				var url = link.attr('href').replace(/#.*$/, '');
 				
 				if($.inArray(url, dont_fetch_again) != -1) {
 					return;
@@ -120,46 +158,23 @@ onready(function(){
 							$(data).find('div[id^="thread_"]').hide().attr('data-cached', 'yes').prependTo('form[name="postcontrols"]');
 						}
 
-						$post = $('[data-board="' + board + '"] div.post#reply_' + id + ', [data-board="' + board + '"]div#thread_' + id);
+						post = $('[data-board="' + board + '"] div.post#reply_' + id + ', [data-board="' + board + '"]div#thread_' + id);
 
-						if(hovering && $post.length > 0) {
-							start_hover($link);
+						if(hovering && post.length > 0) {
+							start_hover(link);
 						}
 					}
 				});
 			}
 		}, function() {
 			hovering = false;
-			if(!$post)
+			if(!post)
 				return;
 			
-			$post.removeClass('highlighted');
-			if($post.hasClass('hidden') || $post.data('cached') == 'yes')
-				$post.css('display', 'none');
+			post.removeClass('highlighted');
+			if(post.hasClass('hidden') || post.data('cached') == 'yes')
+				post.css('display', 'none');
 			$('.post-hover').remove();
-		}).mousemove(function(e) {
-			if(!$post)
-				return;
-			
-			var $hover = $('#post-hover-' + id + '[data-board="' + board + '"]');
-			if($hover.length == 0)
-				return;
-
-			var scrollTop = $(window).scrollTop();
-			if ($link.is("[data-thread]")) scrollTop = 0;
-			var epy = e.pageY;
-			if ($link.is("[data-thread]")) epy -= $(window).scrollTop();			
-
-			var top = (epy ? epy : hovered_at['y']) - 10;
-			
-			if(epy < scrollTop + 15) {
-				top = scrollTop;
-			} else if(epy > scrollTop + $(window).height() - $hover.height() - 15) {
-				top = scrollTop + $(window).height() - $hover.height() - 15;
-			}
-			
-			
-			$hover.css('left', (e.pageX ? e.pageX : hovered_at['x'])).css('top', top);
 		});
 	};
 	
