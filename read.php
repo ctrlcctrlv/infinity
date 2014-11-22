a7d4614a3eeed5f147678458a58204c4564bbd5f
8chan <admin@8chan.co>
2014-11-22 01:46:21 -0800

    Better read.php +50 regex

--- read.php
+++ read.php
@@ -17,7 +17,7 @@ $pages = array(
 	'/(\%b)/' . preg_quote($config['file_index'], '!')					=> 'view_board',
 	'/(\%b)/' . str_replace('%d', '(\d+)', preg_quote($config['file_page'], '!'))		=> 'view_board',
 	'/(\%b)/' . preg_quote($config['dir']['res'], '!') .
-			str_replace('%d', '(\d+)', preg_quote($config['file_page50'], '!'))	=> 'view_thread50',
+			str_replace('%d', '(\d+)', '%d\+50(\.html)?')	=> 'view_thread50',
 	'/(\%b)/' . preg_quote($config['dir']['res'], '!') .
 			str_replace('%d', '(\d+)', '%d(\.html)?')	=> 'view_thread',
 );
