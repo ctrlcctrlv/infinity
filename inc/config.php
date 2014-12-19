22878daecd336ef3d9ff286438d75efb4885ed5b
Forkless <REDACTED@gmail.com>
2014-12-19 06:42:14 -0600

    Added mod function to spoiler all images in a given post

--- inc/config.php
+++ inc/config.php
@@ -1235,6 +1235,7 @@
 	$config['mod']['link_bandelete'] = '[B&amp;D]';
 	$config['mod']['link_deletefile'] = '[F]';
 	$config['mod']['link_spoilerimage'] = '[S]';
+	$config['mod']['link_spoilerimages'] = '[S+]';
 	$config['mod']['link_deletebyip'] = '[D+]';
 	$config['mod']['link_deletebyip_global'] = '[D++]';
 	$config['mod']['link_sticky'] = '[Sticky]';
