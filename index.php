1b29eb5291ef90080c7e594e0154206e207a266b
Fredrick Brennan <admin@8chan.co>
2015-03-11 18:01:42 +0800

    Fix an install bug in index.php

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
 include 'inc/functions.php';
 $query = query('SELECT np.* FROM newsplus np INNER JOIN `posts_news+` p ON np.thread=p.id WHERE np.dead IS FALSE ORDER BY p.bump DESC');
-$newsplus = $query->fetchAll(PDO::FETCH_ASSOC);
+if ($query) {
+	$newsplus = $query->fetchAll(PDO::FETCH_ASSOC);
+} else {
+	$newsplus = false;
+}
 
 $index = Element("8chan/index.html", array("config" => $config, "newsplus" => $newsplus));
 file_write('index.html', $index);
