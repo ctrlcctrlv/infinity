5f1042db9dc303f0d7544534ea71c1040fdca206
Fredrick Brennan <admin@8chan.co>
2015-03-12 14:58:49 +0800

    New stylesheet system

--- js/options/user-css.js
+++ js/options/user-css.js
@@ -1,7 +1,8 @@
 /*
- * options/user-css.js - allow user enter custom css entries
+ * options/user-css.js - allow user to enter custom css entries and (march 2015) replaces the old stylesheet system
  *
  * Copyright (c) 2014 Marcin Łabanowski <marcin@6irc.net>
+ * Copyright (c) 2015 Fredrick Brennan <admin@8chan.co>
  *
  * Usage:
  *   $config['additional_javascript'][] = 'js/jquery.min.js';
@@ -11,10 +12,10 @@
 
 +function(){
 
-var tab = Options.add_tab("user-css", "css3", _("User CSS"));
+var tab = Options.add_tab("user-css", "css3", _("Theme"));
 
 var textarea = $("<textarea></textarea>").css({
-  "height"     : "85%",
+  "height"     : "80%",
   "width"      : "100%",
   "font-size"  : "9pt",
   "font-family": "monospace",
@@ -26,30 +27,102 @@ var submit = $("<input type='button' value='"+_("Save custom CSS")+"'>").css({
   apply_css();
 }).appendTo(tab.content);
 
+onready(function(){
+  var stylechooser = $("<select id='stylechooser'></select>").appendTo(tab.content);
+
+  var fix_choice = function(){
+    stylechooser.empty();
+    $.each(styles, function(k, v) {
+      var ps;
+      k === "Custom" ? k2 = _("Board-specific CSS") : k2 = k;
+      var option = $("<option value='"+k+"'>"+k2+"</option>").appendTo(stylechooser);
+      if (localStorage.stylesheets_all_boards === "false") {
+        var bs = JSON.parse(localStorage.board_stylesheets);
+        if (bs[board_name]) ps = bs[board_name];
+      }
+      if ((k === localStorage.stylesheet && localStorage.stylesheets_all_boards === "true") || (localStorage.stylesheets_all_boards === "false" && (ps && k === ps))) option.prop('selected', 'selected');
+    })
+  };
+
+  fix_choice();
+
+  var allboards = $("<label><input type='checkbox' id='css-all-boards'> "+_("Style should affect all boards, not just current board")+" (/"+board_name+"/)</label>").appendTo(tab.content).find('input');
+
+  // Handle empty localStorage
+  if (!localStorage.stylesheets_all_boards) localStorage.stylesheets_all_boards = "false";
+  if (localStorage.stylesheets_all_boards === "true") allboards.prop('checked', 'checked');
+
+  allboards.on('change', function(e) {
+    if ($(this).is(':checked')) {
+      localStorage.stylesheets_all_boards = "true";
+    } else {
+      localStorage.stylesheets_all_boards = "false";
+    }
+    fix_choice();
+    apply_css();
+  });
+
+  stylechooser.on('change', function(e) {
+    var ps = $(this).val();
+    if (localStorage && localStorage.stylesheets_all_boards === "false") {
+      var bs = JSON.parse(localStorage.board_stylesheets);
+      if (!bs) bs = {};
+      bs[board_name] = ps;
+      localStorage.board_stylesheets = JSON.stringify(bs);
+    } else { 
+      localStorage.stylesheet = ps;
+    }
+
+    apply_css();
+  });
+
+  update_textarea();
+});
+
 var apply_css = function() {
+  var to_apply;
+
+  if (localStorage && localStorage.stylesheets_all_boards === "false") {
+    if (localStorage && localStorage.board_stylesheets) {
+      var bs = JSON.parse(localStorage.board_stylesheets);
+      if (bs && bs[board_name]) {
+        to_apply = styles[bs[board_name]];
+      }
+    } 
+  } else if (localStorage && localStorage.stylesheet) {
+    to_apply = styles[localStorage.stylesheet];
+  }
+
   $('.user-css').remove();
-  $('link[rel="stylesheet"]')
-    .last()
-    .after($("<style></style>")
-      .addClass("user-css")
-      .text(localStorage.user_css)
-    );
+  var ls = $('link[rel="stylesheet"]:not(#stylesheet)').last();
+
+  ls.after($("<style></style>")
+    .addClass("user-css")
+    .text(localStorage.user_css)
+  );
+  
+  if (to_apply) {
+  $('.user-chosen-stylesheet,#stylesheet').remove();
+  ls.after($("<link rel='stylesheet'/>")
+    .attr("class", "user-chosen-stylesheet")
+    .attr("id", "stylesheet")
+    .attr("href", configRoot+to_apply)
+  );
+  }
+  return to_apply;
 };
 
 var update_textarea = function() {
   if (!localStorage.user_css) {
     textarea.text("/* "+_("Enter here your own CSS rules...")+" */\n" +
                   "/* "+_("If you want to make a redistributable style, be sure to\nhave a Yotsuba B theme selected.")+" */\n" +
+                  "/* "+_("These will be applied on top of whatever theme you choose below.")+" */\n" +
                   "/* "+_("You can include CSS files from remote servers, for example:")+" */\n" +
                   '/* @import "http://example.com/style.css"; */');
   }
-  else {
-    textarea.text(localStorage.user_css);
-    apply_css();
-  }
+  textarea.text(localStorage.user_css);
+  apply_css();
 };
-
-update_textarea();
 
 
 }();
