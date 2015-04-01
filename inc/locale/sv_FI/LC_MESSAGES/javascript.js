420e677c08cf0321cd026df9215b1bef66236fc7
8chan <admin@8chan.co>
2015-04-01 16:49:35 -0700

    Generate JS files for locales

---
+++ inc/locale/sv_FI/LC_MESSAGES/javascript.js
@@ -0,0 +1,1 @@
+l10n = {"File":"Fil","URL":"URL","Password":"L\u00f6senord","Delete":"Ta bort","Save":"Spara","Hello!":"Hej!","Options":"Installningar","all":"alla"," ago":"sedan","in ":"i","Auto":"Auto","All":"Alla","second(s)":"sekund(er)","minute(s)":"minut(er)","hour(s)":"tim(ar)","Download All":"H\u00e4mtar alla","(You)":"(Du)","OK":"OK"};
