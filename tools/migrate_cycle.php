d794d88ae0fa36e4735332c1a894bdd9138e3623
8chan <admin@8chan.co>
2015-04-03 18:51:10 -0700

    Information in cycle migration script

--- tools/migrate_cycle.php
+++ tools/migrate_cycle.php
@@ -7,5 +7,6 @@ $boards = listBoards(TRUE);
 #$boards = array('test2');
 
 foreach ($boards as $i => $b) {
+	echo "Processing board $b...";
 	query(sprintf('ALTER TABLE ``posts_%s`` ADD COLUMN cycle INT(1) NOT NULL AFTER locked', $b));
 }
