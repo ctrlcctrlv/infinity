92e4314a6285a21c16eaca7b4e0d6c0ba98e2aac
czaks <marcin@6irc.net>
2015-04-06 22:44:48 +0200

    infinity/smart-build: lol nano

--- inc/instance-functions.php
+++ inc/instance-functions.php
@@ -7,7 +7,7 @@ function max_posts_per_hour($post) {
 	if (!$config['hour_max_threads']) return false;
 
 	if ($post['op']) {
-		$query = prepare(sprintf('SELECT COUNT(*) AS `count` FROM ``posts_%s`` WHERE `thread` IS NULL AND FROM_UNIXTIME(`time`) > DATE_SUB(NOW(), INTERVAL 1 HOUR);', $board[$
+		$query = prepare(sprintf('SELECT COUNT(*) AS `count` FROM ``posts_%s`` WHERE `thread` IS NULL AND FROM_UNIXTIME(`time`) > DATE_SUB(NOW(), INTERVAL 1 HOUR);', $board['uri']));
 		$query->bindValue(':ip', $_SERVER['REMOTE_ADDR']);
 		$query->execute() or error(db_error($query));
 		$r = $query->fetch(PDO::FETCH_ASSOC);
