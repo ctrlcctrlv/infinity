ecfb025021a2da80de9f4788312164a0f53fd0b9
Ron <cipherwraith666@gmail.com>
2017-04-09 23:56:34 +0900

    Cleanup

--- log.php
+++ log.php
@@ -2,23 +2,42 @@
 include 'inc/functions.php';
 include 'inc/mod/pages.php';
 
-if (!isset($_GET['board']) || !preg_match("/{$config['board_regex']}/u", $_GET['board'])) {
-	http_response_code(400);
-	error('Bad board.');
+function outputError($errCode, $errString) {
+  http_response_code($errCode);
+  error ($errString);
+  return 0;
 }
+
+if (!isset($_GET['board'])){
+  outputError(400, 'No input.');
+}
+
+if (!preg_match("/{$config['board_regex']}/u", $_GET['board'])) {
+  outputError(400, 'Bad input.');
+}
+
 if (!openBoard($_GET['board'])) {
-	http_response_code(404);
-	error('No board.');
+  outputError(404, 'No Board.');
 }
 
-if ($board['public_logs'] == 0) error('This board has public logs disabled. Ask the board owner to enable it.');
-if ($board['public_logs'] == 1) $hide_names = false;
-if ($board['public_logs'] == 2) $hide_names = true;
+if ($board['public_logs'] == 0) {
+  outputError(403, 'This board has public logs disabled. Ask the board owner to enable it.');
+}
 
-if (!isset($_GET['page'])) {
-	$page = 1;
-} else {
-	$page = (int)$_GET['page'];
-};
+// Pagination starts at page 1 by default
+$page = 1;
+// Names are hidden by default
+$hideNames = false;
 
-mod_board_log($board['uri'], $page, $hide_names, true);
+// Show names if board is set for public logs
+if ($board['public_logs'] == 2) {
+  $hideNames = true;
+}
+
+// Set pagination if set in query string
+if (isset($_GET['page'])) {
+  $page = (int)$_GET['page'];
+}
+
+// Output mod board log
+mod_board_log($board['uri'], $page, $hideNames, true);
